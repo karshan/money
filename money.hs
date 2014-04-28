e9a96e3c16f6d1aa9222e4a65f86d76bddcd11a7
Karshan Sharma <karshan.sharma@gmail.com>
2014-04-28 16:19:55 -0700

    Cleanup and implemented fuzzyMatch

--- money.hs
+++ money.hs
@@ -1,8 +1,9 @@
 module Main where
 
 import Control.Applicative ((<$>))
-import Data.List (elemIndices)
-import Data.List.Split (splitOn)
+import Control.Arrow (arr, (&&&))
+import Data.Char (isSpace)
+import Data.List (elemIndices, isInfixOf, genericLength, genericTake, genericDrop)
 
 data Transaction = Transaction { description :: String
 							   , date :: String
@@ -14,38 +15,77 @@ data Transaction = Transaction { description :: String
 (<&>) :: Functor f => f a -> (a -> b) -> f b
 (<&>) = flip (<$>)
 
+--TODO rename to |>
+--TODO pick one $ or & don't use both....
 (&) :: a -> (a -> b) -> b
 (&) = flip ($)
 
-window :: Int -> [a] -> [[a]]
+(?) :: Bool -> a -> a -> a
+(?) True  x _ = x
+(?) False _ y = y
+
+unsplit :: (b -> c -> d) -> (b, c) -> d
+unsplit = arr . uncurry
+
+listApp :: (a -> a -> b) -> [a] -> b
+listApp f ls = f (head ls) (last ls)
+
+mapTail :: (a -> a) -> [a] -> [a]
+mapTail f xs = head xs:map f (tail xs)
+
+window :: (Integral n) => n -> [a] -> [[a]]
 window _ [] = []
-window n xs = if length xs < n then [] else (take n xs):window n (tail xs)
+window n xs = if genericLength xs < n then [] else genericTake n xs:window n (tail xs)
 
-substr :: Int -> Int -> String -> String
-substr l u = take (u-l) . drop l 
+substr :: (Integral n) => n -> n -> [a] -> [a]
+substr l u = genericTake (u - l) . genericDrop l
 
-splitOnIndices :: [Int] -> String -> [String]
-splitOnIndices is st = (head out):map (drop 1) (tail out)
-	where
-		out = map (\a -> substr (head a) (last a) st) $ window 2 is
+count :: (Eq a, Integral n) => a -> [a] -> n
+count x = genericLength . filter (==x)
 
+--TODO generalize String to [a]
+--TODO flip some burgers and get rid of the lambda
+--WTF (2 :: Int) are you fucking kidding me....... warning otherwise....
+splitOnIndices :: (Integral n) => [n] -> [a] -> [[a]]
+splitOnIndices is xs = window (2 :: Int) is & map (\a -> listApp substr a xs) & mapTail (genericDrop (1 :: Int))
+
+--TODO generalize ?
+--WTF :: Int.... warning otherwise. need to find better workaround
 splitOnNonEscapedCommas :: String -> [String]
-splitOnNonEscapedCommas s = splitOnIndices splitIndices s
+splitOnNonEscapedCommas str = splitOnIndices splitIndices str
 	where
-		isEscapedCommaIndex idx = take idx s & count '"' & odd
-		count elt st = filter (==elt) st & length
-		splitIndices = 0:(filter (not . isEscapedCommaIndex) (elemIndices ',' s)) ++ [length s]
+		isEscapedCommaIndex idx = odd ((count '"' $ genericTake idx str) :: Int)
+		splitIndices = 0:filter (not . isEscapedCommaIndex) (elemIndices ',' str) ++ [length str]
 
+-- TODO cleanup var names
 csvToTransactions :: String -> [Transaction]
-csvToTransactions s = map (\t -> Transaction {description=(t !! 1), date=(t !! 0), amount=(t !! 2), runningBalance=(t !! 3), tags=[]}) tRecords
+csvToTransactions s = map csvRecordToTransaction tRecords
 	where 
-		header = head csvRecords
 		tRecords = tail csvRecords
-		csvRecords = lines s & splitOn [""] & last & map splitOnNonEscapedCommas
+		csvRecords = lines s & dropWhile (not . all isSpace) & tail & map splitOnNonEscapedCommas
+		-- TODO make this safe, use header = head csvRecords; maybe ?
+		csvRecordToTransaction t = Transaction {description=t !! 1, date=head t, amount=t !! 2, runningBalance=t !! 3, tags=[]}  
 
 --TKTK
 transactions :: IO [Transaction]
-transactions = readFile "C:\\users\\angad\\desktop\\stmt.csv" <&> csvToTransactions
+transactions = readFile "/Users/karshan/gits/money/stmt.csv" <&> csvToTransactions
 
+--Eventually to be used to identify transactions that are the similar and give the same tags to them
+--Maybe a better way to do this would be fuzzy matching
+usefulDescription :: String -> String
+usefulDescription s = 
+	"CHECKCARD" `isInfixOf` head (words s) ? checkCardDescription s $
+	s
+	where
+		checkCardDescription st = clean (words st) & drop 2 & init & unwords
+			where
+				clean ws = if "RECURRING" `isInfixOf` last ws then init ws else ws
+
+--fuzzy string matching for descriptions
+--matches words independently
+--there are warnings here...
+fuzzyMatch :: (Floating a) => String -> String -> a
+fuzzyMatch a b = sequence [words a, words b] & ((fromIntegral . count True . map (listApp (==))) &&& genericLength) & unsplit (\a b -> a/sqrt b)
+		
 main :: IO ()
 main = transactions >>= print
