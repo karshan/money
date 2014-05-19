0a181f34f17b6ab2c594ff5c4dc20f2a10b03f47
Karshan Sharma <karshan.sharma@gmail.com>
2014-05-19 14:08:54 -0700

    Cleanup
     - Moved some functions from Server to Util
     - cleaned similarTransactions code in Server

--- Server.hs
+++ Server.hs
@@ -1,38 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Applicative ((<$>))
-import Control.Monad.Trans.Resource (runResourceT)
-import qualified Data.ByteString.Char8 as BS (unpack, pack, ByteString)
-import qualified Data.ByteString.Lazy.Char8 as LBS (pack, ByteString)
-import Data.Conduit (($$))
-import Data.Conduit.List (consume)
+import Control.Applicative ((<$>), (<*>))
 import Data.Maybe (fromMaybe)
-import Data.Monoid (mconcat)
 import Data.Text (Text)
 import qualified Filesystem.Path.CurrentOS as FP (decodeString)
-import qualified ManageDB as MDB (getTransactions, addTransaction)
-import qualified Money as M (transactions, similarTransactions)
-import Network.HTTP.Types (ok200, movedPermanently301)
-import Network.HTTP.Types.Header (hContentType, hLocation, hContentLength)
-import Network.Wai (Request, Response, responseLBS, pathInfo, requestMethod, requestBody)
+import qualified ManageDB as MDB (getTransactions)
+import qualified Money as M (similarTransactions)
+import Network.HTTP.Types.Header (hContentLength)
+import Network.Wai (Request, Response, pathInfo, requestMethod)
 import Network.Wai.Application.Static (staticApp, defaultWebAppSettings)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Util (mapHeaders)
-import Text.JSON (Result(..), encodeStrict, decodeStrict)
-import Util ((&), (>>>))
-
-rawRequestBody :: Request -> IO BS.ByteString
-rawRequestBody req = mconcat <$> (requestBody req $$ consume)
-
-responseLBS' :: BS.ByteString -> LBS.ByteString -> Response
-responseLBS' c = responseLBS ok200 [(hContentType, c)]
-
-responseString :: String -> Response
-responseString = LBS.pack >>> responseLBS' "text/plain"
-
-responseJSON :: String -> Response
-responseJSON = LBS.pack >>> responseLBS' "application/json"
+import Text.JSON (Result(..), toJSObject)
+import Util ((>>>), (<&>), jsonData, redirect, responseJSON, responseString)
 
 main :: IO ()
 main = putStrLn ("Listening on port " ++ show port) >> run port app
@@ -40,7 +21,7 @@ main = putStrLn ("Listening on port " ++ show port) >> run port app
         port = 3000
 
 app :: Request -> IO Response
-app req = route (pathInfo req) $ req 
+app req = route (pathInfo req) $ req
 
 route :: [Text] -> Request -> IO Response
 route path
@@ -50,26 +31,24 @@ route path
     | path == ["similar"] = similarTransactions
     | otherwise = notFound
 
-redirect :: String -> Request -> IO Response
-redirect url _ = return $ responseLBS movedPermanently301 [(hLocation, BS.pack url)] ""
-
 notFound :: Request -> IO Response
 notFound req = return $ responseString $ "not found " ++ (show $ pathInfo req)
 
--- assumes request pathInfo is non-empty. Verified since its checked in route. 
+-- assumes request pathInfo is non-empty. Verified since its checked in route.
 -- It would be nice if the compiler could make that guarantee.
 static :: Request -> IO Response
-static req = fmap (mapHeaders (filter (\(k,v) -> k /= hContentLength))) $ a (req { pathInfo = tail (pathInfo req) })
+static req = fmap (mapHeaders (filter (\(k,_) -> k /= hContentLength))) $ a (req { pathInfo = tail (pathInfo req) })
     where
         a = staticApp $ defaultWebAppSettings $ FP.decodeString "./static"
 
 transactions :: Request -> IO Response
-transactions _ = MDB.getTransactions >>= (fromMaybe [] >>> encodeStrict >>> responseJSON >>> return)
+transactions _ = MDB.getTransactions <&> (fromMaybe [] >>> responseJSON)
 
 similarTransactions :: Request -> IO Response
 similarTransactions req
-    | requestMethod req == "POST" = rawRequestBody req >>= (BS.unpack >>> decodeStrict >>> f)
+    | requestMethod req == "POST" = jsonData req >>= go
     | otherwise = notFound req
         where
-            f (Ok t) = MDB.getTransactions >>= (fromMaybe [] >>> (flip M.similarTransactions) t >>> encodeStrict >>> responseString >>> return) 
-            f (Error s) = undefined
+            go (Ok t) = (M.similarTransactions <$> ts <*> return t) <&> responseJSON
+            go (Error _) = return $ responseJSON $ toJSObject [("error", "Bad JSON" :: String)]
+            ts = MDB.getTransactions <&> fromMaybe []
--- Util.hs
+++ Util.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Util
     ( (<&>)
     , (>>>)
@@ -9,9 +10,23 @@ module Util
     , substr
     , count
     , splitOnIndices
+    , jsonData
+    , responseString
+    , responseJSON
+    , redirect
     ) where
 
-import Data.Maybe
+import Control.Applicative ((<$>))
+import qualified Data.ByteString.Char8 as BS (ByteString, unpack, pack)
+import qualified Data.ByteString.Lazy.Char8 as LBS (ByteString, pack)
+import Data.Conduit (($$))
+import Data.Conduit.List (consume)
+import Data.Maybe (listToMaybe)
+import Data.Monoid (mconcat)
+import Network.HTTP.Types (ok200, movedPermanently301)
+import Network.HTTP.Types.Header (hContentType, hLocation)
+import Network.Wai (Request, Response, requestBody, responseLBS)
+import Text.JSON (JSON, Result, decodeStrict, encodeStrict)
 
 (<&>) :: Functor f => f a -> (a -> b) -> f b
 (<&>) = flip fmap
@@ -52,3 +67,21 @@ splitOnIndices :: [Int] -> [a] -> [[a]]
 splitOnIndices is xs = window 2 is' & map (\a -> listApp substr a xs) & mapTail (drop (1 :: Int))
     where
         is' = 0:is ++ [length xs]
+
+rawRequestBody :: Request -> IO BS.ByteString
+rawRequestBody req = mconcat <$> (requestBody req $$ consume)
+
+jsonData :: JSON a => Request -> IO (Result a)
+jsonData req = rawRequestBody req <&> (BS.unpack >>> decodeStrict)
+
+responseLBS' :: BS.ByteString -> LBS.ByteString -> Response
+responseLBS' c = responseLBS ok200 [(hContentType, c)]
+
+responseString :: String -> Response
+responseString = LBS.pack >>> responseLBS' "text/plain"
+
+responseJSON :: JSON a => a -> Response
+responseJSON = encodeStrict >>> LBS.pack >>> responseLBS' "application/json"
+
+redirect :: String -> Request -> IO Response
+redirect url _ = return $ responseLBS movedPermanently301 [(hLocation, BS.pack url)] ""
