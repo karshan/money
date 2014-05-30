1168c5037126efcc837e61669339ab9ebc6eb25f
Karshan Sharma <karshan.sharma@gmail.com>
2014-05-30 10:42:40 -0700

    Implemented credit card statement parser

--- ParseCSV.hs
+++ ParseCSV.hs
@@ -1,16 +1,17 @@
 -- Maybe this should be Money.ParseCSV ?
 module ParseCSV
     (
-      debitCardCsvToTransactions
+      parseDebit
+    , parseCredit
     )
     where
 
-import Control.Applicative ((<$>))
+import Control.Applicative ((<$>), (<*>))
 import Data.Char (isSpace)
 import Data.List (elemIndices, isInfixOf)
 import Data.Maybe (mapMaybe)
 import Data.Time (UTCTime(..))
-import Data.Time.Format (parseTime)
+import qualified Data.Time.Format as TF (parseTime)
 import Money (Transaction(..))
 import System.Locale (defaultTimeLocale)
 import Util ((?), maybeRead, splitOnIndices, count, index)
@@ -22,31 +23,48 @@ splitOnNonEscapedCommas str = splitOnIndices splitIndices str
         isEscapedCommaIndex idx = odd (count '"' $ take idx str)
         splitIndices = filter (not . isEscapedCommaIndex) (elemIndices ',' str)
 
+parseAmount :: String -> Maybe Int
+parseAmount a = ((ceiling :: Float -> Int) . (*100)) <$> (maybeRead $ filter (/='"') a)
+parseTime :: String -> Maybe UTCTime
+parseTime = TF.parseTime defaultTimeLocale "%m/%d/%Y"
+
 -- Converts bank of america debit card csv output into [Transaction] Type
 -- TODO cleanup var names
-debitCardCsvToTransactions :: String -> [Transaction]
-debitCardCsvToTransactions s = mapMaybe csvRecordToTransaction tRecords
+parseDebit :: String -> [Transaction]
+parseDebit s = mapMaybe csvRecordToTransaction tRecords
     where
         tRecords = tail csvRecords
         csvRecords = map splitOnNonEscapedCommas $ tail $ dropWhile (not . all isSpace) $ lines s
         -- this code assumes a particular csv header "Date,Description,Amount,Running Bal."
         csvRecordToTransaction :: [String] -> Maybe Transaction
         csvRecordToTransaction t = do
-            _date <- getTime =<< t `index` 0
+            _date <- parseTime =<< t `index` 0
             _description <- t `index` 1
-            _amount <- getAmount =<< t `index` 2
+            _amount <- parseAmount =<< t `index` 2
             return Transaction { description = _description
                                , date = _date
                                , amount = _amount
                                , tags = []
                                }
-        getAmount :: String -> Maybe Int
-        getAmount a = ((ceiling :: Float -> Int) . (*100)) <$> (maybeRead $ filter (/='"') a)
-        getTime :: String -> Maybe UTCTime
-        getTime = parseTime defaultTimeLocale "%m/%d/%Y"
+
+parseCredit :: String -> [Transaction]
+parseCredit s = mapMaybe recordToTransaction records
+    where
+        records = tail $ map splitOnNonEscapedCommas $ lines s
+        -- this code assumes a particular csv header "Posted Date,Reference Number,Payee,Address,Amount"
+        recordToTransaction :: [String] -> Maybe Transaction
+        recordToTransaction t = do
+            _date <- parseTime =<< t `index` 0
+            _description <- (++) <$> t `index` 1 <*> ((:) ' ' <$> t `index` 2)
+            _amount <- parseAmount =<< t `index` 4
+            return Transaction { description = _description
+                               , date = _date
+                               , amount = _amount
+                               , tags = []
+                               }
 
 transactions :: IO [Transaction]
-transactions = debitCardCsvToTransactions <$> readFile "/home/karshan/gits/money/stmt.csv"
+transactions = parseCredit <$> readFile "/home/karshan/gits/money/currentTransaction_3372.csv"
 
 -- The point of this function is to convert transaction descriptions into
 -- usefully unique transactions description. Eventually want something like
