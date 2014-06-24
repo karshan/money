02aebb374d9c0e88b7a56c1b5b2b8d73c4da727b
Karshan Sharma <karshan.sharma@gmail.com>
2014-06-23 17:23:46 -0700

    Cleanup/restructuring

--- static/Main.elm
+++ static/Main.elm
@@ -5,37 +5,41 @@ import Graphics.Input (clickable)
 import Utils (fromMaybe, listFromJson, responseToMaybe, maybeBind, maybeFmap)
 import Transaction (Transaction, transactionFromJson)
 
-main : Signal Element
-main = application <~ state
-
 data State = Start | TList [Transaction] | Edit Transaction
 
-state : Signal State
-state = lift (fromMaybe Start) <| merges stateSignals
+data Nav = Home
 
-stateSignals : [Signal (Maybe State)]
-stateSignals = [ maybeFmap TList <~ transactions
-               , maybeFmap Edit <~ transactionClicks.signal
-               ]
+navI : Input Nav
+navI = input Home
+
+editTransaction : Input (Maybe Transaction)
+editTransaction = input Nothing
+
+main : Signal Element
+main = application <~ appState
+
+appState : Signal State
+appState = lift (fromMaybe Start) <| merges [ maybeFmap TList <~ transactions
+                                            , maybeFmap Edit <~ editTransaction.signal
+                                            ]
 
 transactions : Signal (Maybe [Transaction])
-transactions = let (>>=) = flip maybeBind in
-      (\a -> responseToMaybe a >>= Json.fromString >>= listFromJson transactionFromJson) <~ (sendGet (constant "/transactions"))
+transactions = let (>>=) = flip maybeBind
+                   refreshSignal = keepIf (\a -> a == Home) Home navI.signal in
+      (\a -> responseToMaybe a >>= Json.fromString >>= listFromJson transactionFromJson) <~ sendGet ((\_ -> "/transactions") <~ refreshSignal)
 
-transactionClicks : Input (Maybe Transaction)
-transactionClicks = input Nothing
 
 application : State -> Element
-application st = flow down <| [ nav, container st ]
+application st = flow down <| [ nav st, container st ]
 
-nav : Element
-nav = text "NAVIGATION BAR"
+nav : State -> Element
+nav _ = flow right <| [ clickable navI.handle Home <| text "Home" ]
 
 container : State -> Element
 container st = case st of
-    TList ts -> renderTransactions ts
-    Edit t -> renderTransaction t
-    Start -> asText "START STATE"
+    TList ts -> renderTList ts
+    Edit t -> renderEdit t
+    _ -> text "Gimme a STATE yo!"
 
 text : String -> Element
 text = plainText
@@ -43,13 +47,14 @@ text = plainText
 showAmount : Int -> String
 showAmount a = show (a `div` 100) ++ "." ++ show (a `rem` 100)
 
-renderTransactions : [Transaction] -> Element
-renderTransactions ts = let f x = clickable transactionClicks.handle (Just x) (renderTransaction x) in
-                            flow down <| (text "HEADER")::map f ts
+renderTList : [Transaction] -> Element
+renderTList ts = let renderT t = flow right [ width 100 <| text t.date
+                                            , width 1286 <| text t.description
+                                            , width 80 <| text <| showAmount t.amount
+                                            , width 200 <| text <| "[" ++ (concat <| intersperse ", " t.tags) ++ "]"
+                                            ]
+                     f x = clickable editTransaction.handle (Just x) (renderT x) in
+                            flow down <| {-(text "HEADER")::-}map f ts
 
-renderTransaction : Transaction -> Element
-renderTransaction t = flow right [ width 100 <| text t.date
-                                 , width 1286 <| text t.description
-                                 , width 80 <| text <| showAmount t.amount
-                                 , width 200 <| text <| "[" ++ (concat <| intersperse ", " t.tags) ++ "]"
-                                 ]
+renderEdit : Transaction -> Element
+renderEdit t = asText t
