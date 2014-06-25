af2fcf7952a972a8111e4ebff3c3091627683654
Karshan Sharma <karshan.sharma@gmail.com>
2014-06-25 13:57:01 -0700

    implemented rendering of edit transaction page

--- static/index.js
+++ static/index.js
@@ -19,16 +19,37 @@ function wDiv(w, c, a, s) {
 }
 
 window.onload = function() {
+    function renderNavBar() {
+        return wDiv(1920, fwDiv(100, "TList", [['onclick', 'state(["tlist"]);']]));
+    }
+    $('#navbar').html(renderNavBar());
+
     util.jsonGet("/transactions", function(ts) {
         TList(ts); // TODO handle null error
+        state(["tlist"]);
     });
-}
+};
 
-var TList = $R(function(a) { return a; });
+var TList = $R.state(null);
+var similarTS = $R.state(null);
+var editT = $R.state(null);
 
-function renderTList(ts) {
+var state = $R(function(s) {
+    util.clear();
+    if (s[0] == "tlist") { util.showPage("transaction_list", renderTList(TList())); }
+    else if (s[0] == "edit") {
+        util.jsonPost("/similar", editT(), function(a) { similarTS(a); });
+        util.showPage("edit", renderEdit(editT(), similarTS()));
+    }
+});
+
+var renderTList = function(ts) {
     function showAmount(a) {
         return (a/100) + "";
+    }
+
+    function balance(a) {
+        return _.foldl(a, function(s, e) { return s + e.amount; }, 0);
     }
 
     // renderedts :: [String]
@@ -36,14 +57,38 @@ function renderTList(ts) {
         return wDiv(1920, _.concat([ fwDiv(100, t.date)
                                    , fwDiv(1000, t.description)
                                    , fwDiv(100, showAmount(t.amount))
-                                   , fwDiv(100, JSON.stringify(t.tags), [['onclick', 'trTagClick("' + btoa(JSON.stringify(t)) + '")']])
+                                   , fwDiv(100, JSON.stringify(t.tags), [['onclick', 'trTagClick("' + btoa(JSON.stringify(t)) + '")']], [['cursor', 'pointer']])
                                    ]));
     });
-    $('#transaction_list').html(renderedts);
-}
-$R(renderTList).bindTo(TList)
-
-// balance :: Signal Number
-var balance = $R(function(ts) {
-    return _.foldl(ts, function(s, e) { return s + e.amount; }, 0);
+    return renderedts;
+};
+$R(function(ts) {
+    $('#transaction_list').html(renderTList(ts));
 }).bindTo(TList);
+
+var renderEdit = function(t, ts) {
+    function showAmount(a) {
+        return (a/100) + "";
+    }
+
+    // renderedts :: [String]
+    var renderedts = _.map(ts, function(a) {
+        var t = a[1];
+        return wDiv(1920, _.concat([ fwDiv(100, t.date)
+                                   , fwDiv(1000, t.description)
+                                   , fwDiv(100, showAmount(t.amount))
+                                   , fwDiv(100, JSON.stringify(t.tags))
+                                   , fwDiv(100, a[0])
+                                   ]));
+    });
+    return util.html("div", [], [], JSON.stringify(t)) + util.html("div", [], [], _.concat(renderedts));
+};
+
+$R(function(t, ts) {
+    $('#edit').html(renderEdit(t, ts));
+}).bindTo(editT, similarTS);
+
+var trTagClick = util.jsonFunc(function(t) {
+    editT(t);
+    state(["edit"]);
+});
--- static/util.js
+++ static/util.js
@@ -19,10 +19,44 @@ util.jsonGet = function(url, cb) {
     xhr.send();
 };
 
+util.jsonPost = function(url, d, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (this.status != 200) {
+            cb(null);
+        } else {
+            var json = null;
+            try {
+                json = JSON.parse(this.responseText);
+            } catch(e) {
+                json = null;
+            }
+            cb(json);
+        }
+    };
+    xhr.open("post", url, true);
+    xhr.send(JSON.stringify(d));
+};
+
 util.html = function(tag, attrs, style, contents) {
     return "<" + tag + " " + _.concat(_.map(attrs, function(a) { return a[0] + "=" + a[1] + " "; }))
                + 'style="' + _.concat(_.map(style, function(a) { return a[0] + ": " + a[1] + "; "; })) + '">'
                + contents + "</" + tag + ">";
+};
+
+util.jsonFunc = function(f) {
+    return function(a) {
+        f(JSON.parse(atob(a)));
+    };
+};
+
+util.showPage = function(p, h) {
+    if (h) { $('#' + p).html(h); }
+    $('#' + p).show();
+};
+
+util.clear = function() {
+    $('.page').hide();
 };
 
 _.concat = function(list) {
