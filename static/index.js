be2e589baa05b117f182772935e4d5513a90da91
Karshan Sharma <karshan.sharma@gmail.com>
2014-06-24 18:16:58 -0700

    some cleanup

--- static/index.js
+++ static/index.js
@@ -6,12 +6,16 @@ commonStyle = [ ["height", "18px"]
               , ["text-align", "left"]
               ];
 
-function fwDiv(w, c) {
-    return util.html("div", [], [["width", w + "px"], ["float", "left"]].concat(commonStyle), c);
+function fwDiv(w, c, a, s) {
+    a = a || [];
+    s = s || [];
+    return util.html("div", a, [["width", w + "px"], ["float", "left"]].concat(commonStyle).concat(s), c);
 }
 
-function wDiv(w, c) {
-    return util.html("div", [], [["width", w + "px"]].concat(commonStyle), c);
+function wDiv(w, c, a, s) {
+    a = a || [];
+    s = s || [];
+    return util.html("div", a, [["width", w + "px"]].concat(commonStyle).concat(s), c);
 }
 
 window.onload = function() {
@@ -22,14 +26,24 @@ window.onload = function() {
 
 var TList = $R(function(a) { return a; });
 
-$R(function (ts) { // renderTList
+function renderTList(ts) {
+    function showAmount(a) {
+        return (a/100) + "";
+    }
+
     // renderedts :: [String]
     var renderedts = _.map(ts, function(t) {
         return wDiv(1920, _.concat([ fwDiv(100, t.date)
                                    , fwDiv(1000, t.description)
-                                   , fwDiv(100, t.amount)
-                                   , fwDiv(100, JSON.stringify(t.tags))
+                                   , fwDiv(100, showAmount(t.amount))
+                                   , fwDiv(100, JSON.stringify(t.tags), [['onclick', 'trTagClick("' + btoa(JSON.stringify(t)) + '")']])
                                    ]));
     });
     $('#transaction_list').html(renderedts);
-}).bindTo(TList)
+}
+$R(renderTList).bindTo(TList)
+
+// balance :: Signal Number
+var balance = $R(function(ts) {
+    return _.foldl(ts, function(s, e) { return s + e.amount; }, 0);
+}).bindTo(TList);
