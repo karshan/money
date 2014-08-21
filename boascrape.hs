d1d26a7e82078711f345c2e1163f9c7ffd20b720
Karshan Sharma <karshan.sharma@gmail.com>
2014-08-21 11:55:54 -0700

    boascrape works, integration pending

--- boascrape.hs
+++ boascrape.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Control.Arrow ((&&&))
 import Control.Lens ((&), (.~), (^.))
+import Control.Monad (void)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.State (StateT, get, put, evalStateT)
 import qualified Data.ByteString.Char8 as BS (ByteString, pack, isInfixOf)
 import qualified Data.ByteString.Lazy.Char8 as LBS (ByteString, unpack, writeFile)
 import Data.Default.Class (def)
-import Data.List (isInfixOf)
+import Data.List (intercalate, isPrefixOf)
 import Data.List.Split (splitOn)
+import Data.Maybe (listToMaybe, mapMaybe)
 import Data.Monoid ((<>))
 import Network.Connection (TLSSettings(..))
 import Network.HTTP.Client (CookieJar)
@@ -14,8 +17,7 @@ import Network.HTTP.Client.TLS (mkManagerSettings)
 import Network.Wreq (Options, Response, postWith, getWith, proxy, manager, httpProxy, responseCookieJar, responseBody, cookies)
 import qualified Network.Wreq as W (FormParam((:=)), defaults)
 import Network.Wreq.Types (Postable)
-import Text.XML.HXT.Core
-import Data.Tree.NTree.TypeDefs (NTree)
+import Text.XML.Light
 
 type StateIO s a = StateT s IO a
 
@@ -24,9 +26,6 @@ type StateIO s a = StateT s IO a
 
 defaults :: Options
 defaults = W.defaults & proxy .~ httpProxy "localhost" 8080 & manager .~ Left (mkManagerSettings (TLSSettingsSimple True False False) Nothing)
-
-css :: ArrowXml a => String -> a (NTree XNode) XmlTree
-css tag = multi (hasName tag)
 
 withCookies :: (Options -> IO (Response a)) -> StateIO CookieJar (Response a)
 withCookies f = do
@@ -41,6 +40,15 @@ httppost url params = withCookies (\o -> postWith o url params)
 httpget :: String -> StateIO CookieJar (Response LBS.ByteString)
 httpget url = withCookies (`getWith` url)
 
+toQName :: String -> QName
+toQName s = QName s Nothing Nothing
+
+hasAttrVal :: String -> String -> Element -> Bool
+hasAttrVal k v e = findAttr (toQName k) e == Just v
+
+hasAttrValBy :: String -> (String -> Bool) -> Element -> Bool
+hasAttrValBy k p e = maybe False p $ findAttr (toQName k) e
+
 stateMain :: StateIO CookieJar ()
 stateMain = do
     _ <- httpget "https://www.bankofamerica.com/"
@@ -48,10 +56,11 @@ stateMain = do
     _ <- httppost "https://secure.bankofamerica.com/login/getCAAFSO" [ "pmdata" ^= "" ]
     challengePage <- (^. responseBody) `fmap` httpget "https://secure.bankofamerica.com/login/sign-in/signOn.go"
 
-    let question = BS.pack $ getQuestion $ LBS.unpack challengePage
+    let question = BS.pack `fmap` getQuestion (LBS.unpack challengePage)
     let csrfToken = BS.pack $ getCsrfToken $ LBS.unpack challengePage
+    let sendAnswer q = void $ httppost "https://secure.bankofamerica.com/login/sign-in/validateChallengeAnswer.go" [ "csrfTokenHidden" ^= csrfToken, "challengeQuestionAnswer" ^= getAnswer q, "rembme" ^= "on" ]
 
-    _ <- httppost "https://secure.bankofamerica.com/login/sign-in/validateChallengeAnswer.go" [ "csrfTokenHidden" ^= csrfToken, "challengeQuestionAnswer" ^= getAnswer question, "rembme" ^= "on" ]
+    maybe (return ()) sendAnswer question
     _ <- httppost "https://secure.bankofamerica.com/login/sign-in/validatePassword.go" [ "csrfTokenHidden" ^= csrfToken, "password" ^= password ]
     debit <- httppost "https://secure.bankofamerica.com/myaccounts/details/deposit/download-transactions.go" [ "selectedTransPeriod" ^= ""
                                                                                                          , "downloadTransactionType" ^= "customRange"
@@ -66,10 +75,10 @@ stateMain = do
     lift $ LBS.writeFile "debit.csv" (debit ^. responseBody)
     lift $ LBS.writeFile "credit.csv" (credit ^. responseBody)
     where
-        getQuestion :: String -> String
-        getQuestion body = head (runLA (hread >>> css "label" >>> hasAttrValue "for" (== "tlpvt-challenge-answer") //> getText) body ++ [""])
+        getQuestion :: String -> Maybe String
+        getQuestion body = strContent `fmap` listToMaybe (mapMaybe (filterElement (hasAttrVal "for" "tlpvt-challenge-answer")) $ onlyElems $ parseXML body)
         getCsrfToken :: String -> String
-        getCsrfToken body = head (runLA (hread >>> css "input" >>> hasAttrValue "name" (== "csrfTokenHidden") >>> getAttrValue "value") body ++ [""])
+        getCsrfToken body = fromMaybe (error "getCsrfToken") $ findAttr (toQName "value") =<< listToMaybe (mapMaybe (filterElement (hasAttrVal "name" "csrfTokenHidden")) $ onlyElems $ parseXML body)
         adx = undefined
 
 fromMaybe :: a -> Maybe a -> a
@@ -87,7 +96,15 @@ queryParamsFromUrl :: String -> [(String, String)]
 queryParamsFromUrl = map ((safeHead "" &&& safeLast "") . splitOn "=") . splitOn "&" . safeLast "" . splitOn "?"
 
 getStx :: String -> String
-getStx = undefined
+getStx = fst . head . getStxs
+
+getStxs :: String -> [(String, String)]
+getStxs x = mapMaybe (\e -> do
+    r <- findAttr (toQName "rel") e
+    stx <- lookup "stx" $ queryParamsFromUrl r
+    n <- findAttr (toQName "name") e
+    let date = intercalate "/" $ reverse $ take 3 $ reverse $ splitOn "_" n
+    return (stx, date)) $ concatMap (filterElements $ hasAttrValBy "name" ("goto_transactions_top_for_this_date" `isPrefixOf`)) $ onlyElems $ parseXML x
 
 username :: BS.ByteString
 username = ""
