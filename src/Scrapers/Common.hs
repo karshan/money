2e2b4cf0c10c5dfca2e19019ad3e579a6659f448
Karshan Sharma <karshan.sharma@gmail.com>
2015-11-23 01:34:10 -0800

    BankOfAmerica Scraper Works

--- src/Scrapers/Common.hs
+++ src/Scrapers/Common.hs
@@ -3,15 +3,15 @@ module Scrapers.Common
     ) where
 
 import           Data.List.Split (splitOn)
-import           Data.List.Util  (last, (!!))
-import           Data.Maybe      (fromMaybe, mapMaybe)
+import           Data.List.Util  ((!!))
+import           Data.Maybe      (mapMaybe)
 import           Prelude         hiding (last, (!!))
 
 queryParamsFromUrl :: String -> [(String, String)]
 queryParamsFromUrl url =
-  fromMaybe [] (mapMaybe parseKeyVal . splitOn "&;" <$> last (splitOn "?" url))
-    where
-      parseKeyVal :: String -> Maybe (String, String)
-      parseKeyVal paramString =
-        let xs = splitOn "=" paramString in
-        (,) <$> xs !! 0 <*> xs !! 1
+    mapMaybe parseKeyVal . splitOn "&" $ concat $ tail (splitOn "?" url)
+      where
+        parseKeyVal :: String -> Maybe (String, String)
+        parseKeyVal paramString =
+          let xs = splitOn "=" paramString in
+          (,) <$> xs !! 0 <*> xs !! 1
